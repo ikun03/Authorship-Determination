683cb1f8668793b94557fe53ae5fe2a297eec419
ikun <ikunal03@gmail.com>
2019-04-25 18:26:52 -0400

    Decision tree hopefully done, either it will blow up, or it will run;

--- decision_tree.py
+++ decision_tree.py
@@ -35,17 +35,17 @@ def calculate_threshold(data, i, labels):
     return step_entropy[0]
 
 
-def process_tree_node(decision_tree_node):
+def process_tree_node(data, decision_tree_node):
     # Check if entropy is 0 or there are no stumps to pass
-    node_entropy = dth.entropy(decision_tree_node.labels, decision_tree_node.data)
+    node_entropy = dth.entropy(decision_tree_node.labels, data)
     if node_entropy == 0:
         decision_tree_node.FINAL_LABEL = decision_tree_node.labels[0]
-        return
+        return decision_tree_node
     if len(decision_tree_node.stumps) == 0:
         label_dictionary = {}
         for label in decision_tree_node.labels:
             label_dictionary[label] = 0
-        for data_point in decision_tree_node.data:
+        for data_point in data:
             label_dictionary[data_point[0]] += 1
         max_count = 0
         for label in label_dictionary.keys():
@@ -53,15 +53,65 @@ def process_tree_node(decision_tree_node):
             if val > max_count:
                 max_count = val
                 decision_tree_node.FINAL_LABEL = label
-        return
+        return decision_tree_node
     # Split data to node
     left_node_data = []
     right_node_data = []
-    for data_point in decision_tree_node.data:
+    for data_point in data:
         if data_point[decision_tree_node.att_index] <= decision_tree_node.threshold:
             left_node_data.append(data_point)
         else:
             right_node_data.append(data_point)
+
+    # For left node find the stump with max info gain
+
+    left_node_labels = []
+    for data_point in left_node_data:
+        if data_point[0] not in left_node_labels:
+            left_node_labels.append(data_point[0])
+
+    left_max_info_gain = [0, 0]
+    for att_ind in range(1, len(left_node_data[0])):
+        info_gain = dth.information_gain(left_node_data, left_node_labels, att_ind,
+                                         decision_tree_node.threshold_list[att_ind])
+        if info_gain > left_max_info_gain[1]:
+            left_max_info_gain[0] = att_ind
+            left_max_info_gain[1] = info_gain
+    left_node_stumps = decision_tree_node.stumps
+    left_node_stumps.remove(left_max_info_gain[0])
+
+    left_node = DecisionTree.Node(left_node_stumps, left_max_info_gain[0],
+                                  decision_tree_node.threshold_list[left_max_info_gain[0]],
+                                  decision_tree_node.threshold_list)
+    left_node.labels = left_node_labels
+    left_node = process_tree_node(left_node_data, left_node)
+    decision_tree_node.left = left_node
+
+    # For right node find the stump with max info gain
+
+    right_node_labels = []
+    for data_point in right_node_labels:
+        if data_point[0] not in right_node_labels:
+            right_node_labels.append(data_point[0])
+
+    right_max_info_gain = [0, 0]
+    for att_ind in range(1, len(right_node_data[0])):
+        info_gain = dth.information_gain(right_node_data, right_node_labels, att_ind,
+                                         decision_tree_node.threshold_list[att_ind])
+        if info_gain > right_max_info_gain[1]:
+            right_max_info_gain[0] = att_ind
+            right_max_info_gain[1] = info_gain
+    right_node_stumps = decision_tree_node.stumps
+    right_node_stumps.remove(right_max_info_gain[0])
+
+    right_node = DecisionTree.Node(right_node_stumps, right_max_info_gain[0],
+                                   decision_tree_node.threshold_list[right_max_info_gain[0]],
+                                   decision_tree_node.threshold_list)
+    right_node.labels = right_node_labels
+    right_node = process_tree_node(right_node_data, right_node)
+    decision_tree_node.right = right_node
+
+    return decision_tree_node
 
 
 class DecisionTree:
@@ -73,9 +123,9 @@ class DecisionTree:
         threshold_list = {}
         for i in range(1, len(data[0])):
             threshold_list[i] = calculate_threshold(data, i, labels)
-        max_info_gain = [0, 0]
 
         # Get the attribute with max info gain that shall be our root
+        max_info_gain = [0, 0]
         for att_ind in range(1, len(data[0])):
             info_gain = dth.information_gain(data, labels, att_ind, threshold_list[att_ind])
             if info_gain > max_info_gain[1]:
@@ -89,18 +139,18 @@ class DecisionTree:
                 stumps.append(att_ind)
 
         # Creating the root node
-        decision_tree = DecisionTree.Node(data, stumps, max_info_gain[0], threshold_list[max_info_gain[0]],
+        decision_tree = DecisionTree.Node(stumps, max_info_gain[0], threshold_list[max_info_gain[0]],
                                           threshold_list)
-        process_tree_node(decision_tree)
+        decision_tree.labels = labels
+        process_tree_node(data, decision_tree)
 
     class Node:
-        __slots__ = "data", "left", "right", "stumps", "att_index", "threshold", "labels", "threshold_list", "FINAL_LABEL"
+        __slots__ = "left", "right", "stumps", "att_index", "threshold", "labels", "threshold_list", "FINAL_LABEL"
 
-        def __init__(self, values, stumps, attribute_index, threshold, attribute_thresholds):
-            self.data = values
+        def __init__(self, stumps, attribute_index, threshold, attribute_thresholds):
             self.stumps = stumps
-            self.left = []
-            self.right = []
+            self.left = None
+            self.right = None
             self.att_index = attribute_index
             self.threshold = threshold
             self.labels = ""
