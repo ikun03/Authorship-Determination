d7f935abbb17753182827859cfd0ceae0c0cf2f3
ikun <ikunal03@gmail.com>
2019-04-26 02:40:23 -0400

    Data processing nearly done

--- file_processing.py
+++ file_processing.py
@@ -9,65 +9,66 @@ nltk.download('stopwords')
 
 
 def process(fileName, author):
+    dataset_row = [author]
     print("**************** Processing file: " + fileName + "****************")
     file = open(fileName, "r")
     sentences = nltk.sent_tokenize(file.read())
-    wordDictionary = {}
-    operandDictionary = {}
-    hyphenatedWordsDictionary = {}
-    lineCount = 0
-    partSpeechDict = {}
+    word_dictionary = {}
+    operand_dictionary = {}
+    hyphenated_words_dictionary = {}
+    line_count = 0
+    part_speech_dict = {}
     for line in sentences:
-        lineCount += 1
-        wordsList = re.findall("[A-Za-z]+-[A-Za-z]+|[A-Za-z]+", line)
-        operandsList = re.findall("[^A-Za-z\s]", line)
-        hyphenatedWordsList = re.findall("[A-Za-z]+-[A-Za-z]+", line)
-        partSpeech = nltk.pos_tag(nltk.word_tokenize(line))
+        line_count += 1
+        words_list = re.findall("[A-Za-z]+-[A-Za-z]+|[A-Za-z]+", line)
+        operands_list = re.findall("[^A-Za-z\s]", line)
+        hyphenated_words_list = re.findall("[A-Za-z]+-[A-Za-z]+", line)
+        part_speech = nltk.pos_tag(nltk.word_tokenize(line))
 
-        for part in partSpeech:
+        for part in part_speech:
             key = part[1]
-            if key in partSpeechDict.keys():
-                partSpeechDict[key] = partSpeechDict[key] + 1
+            if key in part_speech_dict.keys():
+                part_speech_dict[key] = part_speech_dict[key] + 1
             else:
-                partSpeechDict[key] = 1
+                part_speech_dict[key] = 1
 
-        for word in wordsList:
-            if word.lower() in wordDictionary.keys():
-                wordDictionary[word.lower()] = wordDictionary[word.lower()] + 1
+        for word in words_list:
+            if word.lower() in word_dictionary.keys():
+                word_dictionary[word.lower()] = word_dictionary[word.lower()] + 1
             else:
-                wordDictionary[word.lower()] = 1
+                word_dictionary[word.lower()] = 1
 
-        for hyp in hyphenatedWordsList:
-            if hyp.lower() in hyphenatedWordsDictionary.keys():
-                hyphenatedWordsDictionary[hyp.lower()] = hyphenatedWordsDictionary[hyp.lower()] + 1
+        for hyp in hyphenated_words_list:
+            if hyp.lower() in hyphenated_words_dictionary.keys():
+                hyphenated_words_dictionary[hyp.lower()] = hyphenated_words_dictionary[hyp.lower()] + 1
             else:
-                hyphenatedWordsDictionary[hyp.lower()] = 1
+                hyphenated_words_dictionary[hyp.lower()] = 1
 
-        for operand in operandsList:
-            if operand in operandDictionary.keys():
-                operandDictionary[operand] = operandDictionary[operand] + 1
+        for operand in operands_list:
+            if operand in operand_dictionary.keys():
+                operand_dictionary[operand] = operand_dictionary[operand] + 1
             else:
-                operandDictionary[operand] = 1
+                operand_dictionary[operand] = 1
 
-    sorted_word_dictionary = sorted(wordDictionary.items(), key=lambda kv: kv[1], reverse=True)
-    sorted_operand_dictionary = sorted(operandDictionary.items(), key=lambda kv: kv[1], reverse=True)
-    sorted_part_dictionary = sorted(partSpeechDict.items(), key=lambda kv: kv[1], reverse=True)
+    sorted_word_dictionary = sorted(word_dictionary.items(), key=lambda kv: kv[1], reverse=True)
+    sorted_operand_dictionary = sorted(operand_dictionary.items(), key=lambda kv: kv[1], reverse=True)
+    sorted_part_dictionary = sorted(part_speech_dict.items(), key=lambda kv: kv[1], reverse=True)
 
-    bannedList = set(stopwords.words('english'))
+    banned_list = set(stopwords.words('english'))
 
     sorted_word_dictionary_copy = sorted_word_dictionary.copy()
     for i in range(0, len(sorted_word_dictionary_copy)):
-        if sorted_word_dictionary_copy[i][0] in bannedList:
+        if sorted_word_dictionary_copy[i][0] in banned_list:
             sorted_word_dictionary.remove(sorted_word_dictionary_copy[i])
 
-    bannedOperands = []
+    banned_operands = []
 
     sorted_operand_dictionary_copy = sorted_operand_dictionary.copy()
     for i in range(0, len(sorted_operand_dictionary_copy)):
-        if sorted_operand_dictionary_copy[i][0] in bannedOperands:
+        if sorted_operand_dictionary_copy[i][0] in banned_operands:
             sorted_operand_dictionary.remove(sorted_operand_dictionary_copy[i])
 
-    mean_word_dictionary = sorted(wordDictionary.items(), key=lambda kv: len(kv[0]))
+    mean_word_dictionary = sorted(word_dictionary.items(), key=lambda kv: len(kv[0]))
     median_tuple = mean_word_dictionary[len(mean_word_dictionary) // 2]
     print("Median word length: " + str(len(median_tuple[0])))
     print("Median word : " + median_tuple[0])
@@ -81,7 +82,7 @@ def process(fileName, author):
         mean_length_sentence += len(sentence)
     print("Mean Sentence length: " + str(mean_length_sentence // len(sorted_sentences)))
 
-    mean_word_dictionary = sorted(wordDictionary.items(), key=lambda kv: len(kv[0]))
+    mean_word_dictionary = sorted(word_dictionary.items(), key=lambda kv: len(kv[0]))
     mean = 0
     for word in mean_word_dictionary:
         mean += len(word[0])
@@ -89,44 +90,51 @@ def process(fileName, author):
 
     words_file = open(fileName + "word_results.txt", 'w')
     for tuple in sorted_word_dictionary:
-        words_file.write(tuple[0] + " : " + str(tuple[1]) + " : " + str(round(tuple[1] / lineCount, 4)) + "\n\n")
+        words_file.write(tuple[0] + " : " + str(tuple[1]) + " : " + str(round(tuple[1] / line_count, 4)) + "\n\n")
 
     words_file = open(fileName + "operand_results.txt", 'w')
     for tuple in sorted_operand_dictionary:
-        words_file.write(tuple[0] + " : " + str(tuple[1]) + " : " + str(round(tuple[1] / lineCount, 4)) + "\n\n")
+        words_file.write(tuple[0] + " : " + str(tuple[1]) + " : " + str(round(tuple[1] / line_count, 4)) + "\n\n")
 
     words_file = open(fileName + "parts_of_speech_result.txt", 'w')
     for tuple in sorted_part_dictionary:
-        words_file.write(tuple[0] + " : " + str(tuple[1]) + " : " + str(round(tuple[1] / lineCount, 4)) + "\n\n")
+        words_file.write(tuple[0] + " : " + str(tuple[1]) + " : " + str(round(tuple[1] / line_count, 4)) + "\n\n")
 
-    hypWordCount = len(hyphenatedWordsDictionary.keys())
-    print("hyphenated words : " + str(hypWordCount) + " : " + str(round(hypWordCount / lineCount, 4)))
+    hyp_word_count = len(hyphenated_words_dictionary.keys())
+    print("hyphenated words : " + str(hyp_word_count) + " : " + str(round(hyp_word_count / line_count, 4)))
 
     print("**************** Ending file process: " + fileName + "****************")
+    dataset_row.append(round(operand_dictionary[";"] / line_count, 4))
+    dataset_row.append(round(operand_dictionary[","] / line_count, 4))
+    dataset_row.append(round(part_speech_dict["RB"] / line_count, 4))
+    dataset_row.append(round(mean_length_sentence / len(sorted_sentences), 4))
+    dataset_row.append(round(operand_dictionary["?"] / line_count, 4))
+    return dataset_row
 
 
 data_set = []
-process("lost_world.txt", "ACD")
-process("sherlock.txt", "ACD")
-process("study_in_scarlet.txt", "ACD")
-process("moby_dick.txt", "HM")
-process("bartleby.txt", "HM")
-process("confidence_man.txt", "HM")
-process("pierre.txt", "HM")
-process("white_jacket.txt", "HM")
-process("typee.txt", "HM")
-process("battle_pieces.txt", "HM")
-process("redburn.txt", "HM")
-process("omoo.txt", "HM")
-process("israel_potter.txt", "HM")
-process("my_chimney.txt", "HM")
-process("mardi.txt", "HM")
-process("baskervilles.txt", "ACD")
-process("sing_four.txt", "ACD")
-process("return.txt", "ACD")
-process("memoirs.txt", "ACD")
-process("valley.txt", "ACD")
-process("tales_terror.txt", "ACD")
-process("white_company.txt", "ACD")
-process("last_bow.txt", "ACD")
-process("boer_war", "ACD")
+data_set.append(process("lost_world.txt", "ACD"))
+data_set.append(process("sherlock.txt", "ACD"))
+data_set.append(process("study_in_scarlet.txt", "ACD"))
+data_set.append(process("moby_dick.txt", "HM"))
+data_set.append(process("bartleby.txt", "HM"))
+data_set.append(process("confidence_man.txt", "HM"))
+data_set.append(process("pierre.txt", "HM"))
+data_set.append(process("white_jacket.txt", "HM"))
+data_set.append(process("typee.txt", "HM"))
+data_set.append(process("battle_pieces.txt", "HM"))
+data_set.append(process("redburn.txt", "HM"))
+data_set.append(process("omoo.txt", "HM"))
+data_set.append(process("israel_potter.txt", "HM"))
+data_set.append(process("my_chimney.txt", "HM"))
+data_set.append(process("mardi.txt", "HM"))
+data_set.append(process("baskervilles.txt", "ACD"))
+data_set.append(process("sign_four.txt", "ACD"))
+data_set.append(process("return.txt", "ACD"))
+data_set.append(process("memoirs.txt", "ACD"))
+data_set.append(process("valley.txt", "ACD"))
+data_set.append(process("tales_terror.txt", "ACD"))
+data_set.append(process("white_company.txt", "ACD"))
+data_set.append(process("last_bow.txt", "ACD"))
+data_set.append(process("boer_war.txt", "ACD"))
+print(data_set)
